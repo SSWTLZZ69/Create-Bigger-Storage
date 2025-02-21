3603fbbf3e8b61cbdd66720fe0ca6c49fd389325
xuwu / luna <loganalexzand@gmail.com>
2025-02-21 03:41:37 -0900

    Fix Mixin & Bump

--- src/main/java/uwu/lopyluna/create_bs/mixins/MountedStorageMixin.java
+++ src/main/java/uwu/lopyluna/create_bs/mixins/MountedStorageMixin.java
@@ -1,8 +1,7 @@
 package uwu.lopyluna.create_bs.mixins;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.simibubi.create.content.contraptions.MountedStorage;
+import com.simibubi.create.content.logistics.crate.BottomlessItemHandler;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.items.ItemStackHandler;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,7 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import uwu.lopyluna.create_bs.content.vault.TieredVaultBlockEntity;
 
-import static uwu.lopyluna.create_bs.BSUtils.returnMixin;
 
 @Mixin(value = MountedStorage.class, remap = false)
 public class MountedStorageMixin {
@@ -21,21 +19,23 @@ public class MountedStorageMixin {
     @Shadow private BlockEntity blockEntity;
     @Shadow ItemStackHandler handler;
     @Shadow boolean valid;
+    @Shadow boolean noFuel;
 
+    @SuppressWarnings("all")
     @Inject(at = @At("HEAD"), method = "canUseAsStorage(Lnet/minecraft/world/level/block/entity/BlockEntity;)Z", cancellable = true)
-    private static void canUseAsStorage(BlockEntity be, CallbackInfoReturnable<Boolean> cir) {
-        if (be instanceof TieredVaultBlockEntity)
-            returnMixin(true, cir);
+    private static void BS$canUseAsStorage(BlockEntity be, CallbackInfoReturnable<Boolean> cir) {
+        if (be != null && be instanceof TieredVaultBlockEntity)
+            cir.setReturnValue(true);
     }
 
-    @WrapOperation(method = "<init>(Lnet/minecraft/world/level/block/entity/BlockEntity;)V", at = @At(value = "CONSTANT", args = "classValue=com/simibubi/create/content/logistics/vault/ItemVaultBlockEntity"))
-    private boolean MountedStorage(Object object, Operation<Boolean> original) {
-        return original.call(object) || object instanceof TieredVaultBlockEntity;
+    @Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/world/level/block/entity/BlockEntity;)V")
+    private void BS$MountedStorage(BlockEntity be, CallbackInfo ci) {
+        noFuel = noFuel || be instanceof TieredVaultBlockEntity;
     }
 
     @Inject(at = @At("HEAD"), method = "removeStorageFromWorld()V", cancellable = true)
-    private void removeStorageFromWorld(CallbackInfo ci) {
-        if (blockEntity instanceof TieredVaultBlockEntity be) {
+    private void BS$removeStorageFromWorld(CallbackInfo ci) {
+        if (blockEntity != null && blockEntity instanceof TieredVaultBlockEntity be) {
             handler = be.getInventoryOfBlock();
             valid = true;
             ci.cancel();
@@ -43,8 +43,8 @@ public class MountedStorageMixin {
     }
 
     @Inject(at = @At("HEAD"), method = "addStorageToWorld(Lnet/minecraft/world/level/block/entity/BlockEntity;)V", cancellable = true)
-    private void addStorageToWorld(BlockEntity blockEntity, CallbackInfo ci) {
-        if (blockEntity instanceof TieredVaultBlockEntity be) {
+    private void BS$addStorageToWorld(BlockEntity blockEntity, CallbackInfo ci) {
+        if (!(handler instanceof BottomlessItemHandler) && blockEntity instanceof TieredVaultBlockEntity be) {
             be.applyInventoryToBlock(handler);
             ci.cancel();
         }
